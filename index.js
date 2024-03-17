abfaff89d0594b4d0e3261e21d7ee0f808a5a006
selvakisar <130025654+selvakisar@users.noreply.github.com>
2024-03-17 22:13:38 +0530

    bug

--- index.js
+++ index.js
@@ -1,38 +1,37 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import userRoutes from './routes/user.js';
-import chatRoutes from './routes/chat.js';
-import messageRoutes from './routes/message.js';
-import * as Server from 'socket.io';
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+import userRoutes from "./routes/user.js";
+import chatRoutes from "./routes/chat.js";
+import messageRoutes from "./routes/message.js";
+import * as Server from "socket.io";
 
-
-dotenv.config()
+dotenv.config();
 const app = express();
 const corsConfig = {
   origin: "http://localhost:3000",
   credentials: true,
 };
-const PORT=process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
-
-mongoose.connect(process.env.MONGODB_URI )
+mongoose
+  .connect(process.env.MONGODB_URI)
   .then(() => {
-    console.log('Connected to MongoDB Atlas');
+    console.log("Connected to MongoDB Atlas");
   })
-  .catch(error => {
-    console.error('Error connecting to MongoDB Atlas:', error.message);
-  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB Atlas:", error.message);
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors(corsConfig));
-app.use('/', userRoutes);
-app.use('/api/chat', chatRoutes);
-app.use('/api/message', messageRoutes);
-mongoose.set('strictQuery', false);
+app.use("/", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
+mongoose.set("strictQuery", false);
 
 const server = app.listen(PORT, () => {
   console.log(`Server Listening at PORT - ${PORT}`);
@@ -40,26 +39,26 @@ const server = app.listen(PORT, () => {
 const io = new Server.Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: 'http://localhost:3000',
+    origin: "http://localhost:3000",
   },
 });
-io.on('connection', (socket) => {
-  socket.on('setup', (userData) => {
+io.on("connection", (socket) => {
+  socket.on("setup", (userData) => {
     socket.join(userData.id);
-    socket.emit('connected');
+    socket.emit("connected");
   });
-  socket.on('join room', (room) => {
+  socket.on("join room", (room) => {
     socket.join(room);
   });
-  socket.on('typing', (room) => socket.in(room).emit('typing'));
-  socket.on('stop typing', (room) => socket.in(room).emit('stop typing'));
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
-  socket.on('new message', (newMessageRecieve) => {
+  socket.on("new message", (newMessageRecieve) => {
     var chat = newMessageRecieve.chatId;
-    if (!chat.users) console.log('chats.users is not defined');
+    if (!chat.users) console.log("chats.users is not defined");
     chat.users.forEach((user) => {
       if (user._id == newMessageRecieve.sender._id) return;
-      socket.in(user._id).emit('message recieved', newMessageRecieve);
+      socket.in(user._id).emit("message recieved", newMessageRecieve);
     });
   });
 });
